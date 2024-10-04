f1900db89dcef185a0a70a0202537c268555c592
William Brown <wsbrown.colo@gmail.com>
2024-10-04 14:47:52 -0600

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+		int birthYear = 1990;
+		String month = "November";
+		Boolean weekend = false;
+		System.out.println("Birth Year: "+ birthYear);
+		System.out.println("Month: "+ month);
+		System.out.println("Today is the weekend: "+ weekend);
+	}
+}
