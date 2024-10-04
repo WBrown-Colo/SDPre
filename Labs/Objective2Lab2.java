8e91687ae459fc97cefacff260f56cd106db3a0b
William Brown <wsbrown.colo@gmail.com>
2024-10-04 14:34:46 -0600

    complete objective2lab2

--- Labs/Objective2Lab2.java
+++ Labs/Objective2Lab2.java
@@ -3,5 +3,8 @@ public class Objective2Lab2 {
 		String name = "William Brown";
 		int age = 33;
 		String state = "Colorado";
+		System.out.println("Name:"+ name);
+		System.out.println("Age:"+ age);
+		System.out.println("State:"+ state);
 	}
 }
