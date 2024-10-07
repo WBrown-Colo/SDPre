2d005a900821a59d0ca32a52384dc0de73c4b8d6
William Brown <wsbrown.colo@gmail.com>
2024-10-07 12:04:39 -0600

    complete objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,9 @@
+public class Objective3Lab2 {
+	public static void main(String[] args) {
+		double side1 = 10;
+		double side2 = 8;
+		double hypotenuse = Math.sqrt((side1 * side1) + (side2 * side2));
+		System.out.println("The hypotenuse of a triangle with sides 10 and 8 is");
+		System.out.println(hypotenuse);
+	}
+}
